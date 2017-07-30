13a51301eaebc47f5ebcc1bbbdcda92f048b891f
doterlin123 <453845311@qq.com>
2017-07-30 23:27:51 +0800

    登陆测试

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+    waitTimeout: 60 * 1000
+});
 var login = require('./src/lib/login');
 var utils = require('utils');
 
 casper.start('https://wx.qq.com/');
 
+//login
 casper.then(function() {
     var ts = this;
     var qrUrl = ts.getElementAttribute('.qrcode .img', 'src');
@@ -13,6 +16,27 @@ casper.then(function() {
         login.start(ts);
     });
 });
+
+
+//loggin success
+casper.waitForSelector('body.loaded', function() {
+    this.captureSelector('./static/img/login_success.png', 'html');
+    console.log('登陆成功！')
+});
+
+
+// loggin success
+// casper.waitForSelector('body.loaded', function() {
+//     this.captureSelector('./static/img/login_success.png', 'html');
+//     console.log('登陆成功！')
+// });
+
+
+//loggin error
+casper.waitForSelector('.tweet-row', function() {
+    this.captureSelector('./static/img/login_err.png', 'html');
+});
+
 
 // casper.thenOpen('http://phantomjs.org', function() {
 //     this.echo('Second Page: ' + this.getTitle());
